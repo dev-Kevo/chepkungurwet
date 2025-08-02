2494377ec11b8a4d91e87b0dabfa54de57617cf7
dev-kevo <developerkevo@gmail.com>
2025-08-02 17:28:33 +0300

    corrected database settings

--- kunguruwet/settings.py
+++ kunguruwet/settings.py
@@ -101,8 +101,8 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME', 'kunguruwet'),
-            'USER': os.getenv('DB_USER', 'kunguruwet_db'),
+            'NAME': os.getenv('DB_NAME', 'kunguruwet_db'),
+            'USER': os.getenv('DB_USER', 'kunguruwet'),
             'PASSWORD': os.getenv('DB_PASSWORD', 'Chepkunguruwet@2025'),
             'HOST': os.getenv('DB_HOST', 'localhost'),
             'PORT': os.getenv('DB_PORT', '5432'),
