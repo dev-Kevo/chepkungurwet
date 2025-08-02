f4df26e873edb1074927bd24ec6e9792aaf4ffdd
dev-kevo <developerkevo@gmail.com>
2025-08-02 17:12:19 +0300

    Updated database settings

--- kunguruwet/settings.py
+++ kunguruwet/settings.py
@@ -89,12 +89,25 @@ WSGI_APPLICATION = 'kunguruwet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME', 'kunguruwet'),
+            'USER': os.getenv('DB_USER', 'kunguruwet_db'),
+            'PASSWORD': os.getenv('DB_PASSWORD', 'Chepkunguruwet@2025'),
+            'HOST': os.getenv('DB_HOST', 'localhost'),
+            'PORT': os.getenv('DB_PORT', '5432'),
+        }
+    }
 
 
 # Password validation
