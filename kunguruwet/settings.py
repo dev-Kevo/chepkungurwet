bcc54ab8fcca94ae8afa264dda15bba998199b69
dev-kevo <developerkevo@gmail.com>
2025-08-02 20:02:23 +0300

    Added tailwindCSS static folder

--- kunguruwet/settings.py
+++ kunguruwet/settings.py
@@ -158,6 +158,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'theme', 'static'), 
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
